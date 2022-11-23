c3b47a2cacbaec680d4e3a1d07d007be08f2047a
spark035 <63474921+spark035@users.noreply.github.com>
2022-11-23 18:59:02 -0500

    basic routing done

--- cruise-ship-web-application/src/app/app-routing.module.ts
+++ cruise-ship-web-application/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AmenitiesComponent } from './components/amenities/amenities.component';
+import { CateringComponent } from './components/catering/catering.component';
+import { OrderCateringComponent } from './components/order-catering/order-catering.component';
+import { BookAmenityComponent } from './components/book-amenity/book-amenity.component';
+import { ModifyCateringComponent } from './components/modify-catering/modify-catering.component';
+import { ChangeBookingComponent } from './components/change-booking/change-booking.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'amenities/book', component: BookAmenityComponent},
+  {path: 'amenities/change', component: ChangeBookingComponent},
+  {path: 'amenities/edit', component: AmenitiesComponent},
+  {path: 'catering/order', component: OrderCateringComponent},
+  {path: 'catering/modify', component: ModifyCateringComponent},
+  {path: 'catering/edit', component: CateringComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
